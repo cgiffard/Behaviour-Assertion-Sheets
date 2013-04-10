da4f1d1b59666b3eabaad970e29cb8c6490c76ce
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-04-10 14:40:49 +1000

    Test runner stubbed.

--- rule-parse-init.js
+++ rule-parse-init.js
@@ -1,4 +1,8 @@
-var BAS = require("./"),
+var request = require("request"),
+	BAS = require("./"),
 	testSuite = new BAS();
 	
 testSuite.loadSheet(__dirname + "/test/sheets/github.bas");
+request("https://github.com/",function(err,res,body) {
+	testSuite.run("https://github.com/",res,body);
+});
