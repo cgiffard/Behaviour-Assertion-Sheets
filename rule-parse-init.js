7f00ac248a71eef096638f31ee520d6369a2b9bc
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-04-16 10:15:49 +1000

    Assertions underway.

--- rule-parse-init.js
+++ rule-parse-init.js
@@ -12,10 +12,32 @@ request
 	});
 
 testSuite
-	.addTest(function() {
-		
-	})
-
-testSuite
 	.loadSheet(__dirname + "/test/sheets/github.bas")
 	.yep(request.last);
+
+testSuite.on("start",function() {
+	console.log("Starting test suite...");
+});
+
+testSuite.on("testregistered",function(name,func) {
+	console.log("A new test, '%s', was registered.",name);
+});
+
+testSuite.on("startgroup",function(rule) {
+	console.log("Starting test group: ",rule);
+});
+
+testSuite.on("testassertion",function(assertion) {
+	console.log("Testing assertion ",assertion);
+});
+
+testSuite.on("assertionfailed",function(assertion) {
+	console.log("Assertion failed: ",assertion);
+});
+
+testSuite.on("end",function() {
+	console.log("Test Suite Completed. Errors:");
+	testSuite.errors.forEach(function(error) {
+		console.log(error);
+	});
+});
