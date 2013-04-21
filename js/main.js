9e3d2a0d7cb585c4f8d09be5e4e404e9b590bdb6
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-04-21 20:26:55 +1000

    Updated to fix link behaviour and make toolbar less annoying

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@
 	function onload() {
 		
 		var navBar = document.querySelector("nav"),
-			nodePointer = navBar,
 			positionTop = 0;
 			
 		resetSizing();
@@ -25,13 +24,49 @@
 		}
 		
 		function resetSizing() {
-			nodePointer = navBar;
-			positionTop = 0;
-			
-			do {
-				positionTop += nodePointer.offsetTop;
-			} while (nodePointer = nodePointer.offsetParent);
+			positionTop = getElementOffset(navBar);
 		}
+		
+		alterLinkBehaviour();
+	}
+	
+	function alterLinkBehaviour() {
+		var links = [].slice.call(document.querySelectorAll("nav a"),0);
+		
+		links.forEach(function(link) {
+			if (link.getAttribute("href").indexOf("#") === 0) {
+				link.addEventListener("click",linkHandler);
+				link.addEventListener("keydown",linkHandler);
+			}
+		});
+		
+		function linkHandler(evt) {
+			if (evt.target !== this) return true;
+			
+			if (evt.preventDefault) {
+				evt.preventDefault();
+			}
+			
+			var target = document.querySelector(this.getAttribute("href"))
+				targetOffset = getElementOffset(target);
+			
+			document.body.scrollTop = targetOffset - 100;
+			
+			return false;
+		}
+	}
+	
+	function getElementOffset(element) {
+		var nodePointer = element,
+			positionTop = 0;
+		
+		if (!element) return 0;
+		
+		do {
+			positionTop += nodePointer.offsetTop;
+		} while ((nodePointer = nodePointer.offsetParent));
+		
+		return positionTop;
 	}
 	
 })();
