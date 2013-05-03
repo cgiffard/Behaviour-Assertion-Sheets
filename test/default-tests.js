3c097ad67587fb52dce6e94264b4699068ec3334
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-05-03 22:56:19 +1000

    Fixed a bug in the port and statuscode tests. Completed test suite for assertion tests. Port assertion test was actually returning the protocol, embarrassingly enough. And the status code test worked, but failed if the header block was missing - which probably wasn't a problem but I decided to make the code more robust anyway.

--- test/default-tests.js
+++ test/default-tests.js
@@ -2,7 +2,7 @@ var cheerio = require("cheerio"),
 	chai = require("chai");
 	chai.should();
 
-describe("Default assertion tests",function() {
+describe("Default assertion test",function() {
 
 	var tests = require("../lib/tests.js");
 
@@ -12,119 +12,220 @@ describe("Default assertion tests",function() {
 				"document": cheerio.load("<html><title>Example title to extract</title></html>")
 			};
 			
-			tests.title(documentState).should.equal("Example title to extract");
+			tests.title(documentState)
+				.should.equal("Example title to extract");
 		});
 	});
-});
+	
+	describe("url",function() {
+		it("should return the url of the document",function() {
+			var documentState = {
+				"url": "http://test.com:3000/fish?stuff=otherstuff"
+			};
+			
+			tests.url(documentState)
+				.should.equal(documentState.url);
+		});
+	});
+	
+	describe("domain",function() {
+		it("should return domain component from the url of the document",function() {
+			var documentState = {
+				"url": "http://test.com:3000/fish?stuff=otherstuff"
+			};
+			
+			tests.domain(documentState)
+				.should.equal("test.com");
+		});
+	});
+	
+	describe("protocol",function() {
+		it("should return protocol component from the url of the document",function() {
+			var documentState = {
+				"url": "http://test.com:3000/fish?stuff=otherstuff"
+			};
+			
+			tests.protocol(documentState)
+				.should.equal("http");
+		});
+	});
+	
+	describe("port",function() {
+		it("should return port component from the url of the document",function() {
+			var documentState = {
+				"url": "http://test.com:3000/fish?stuff=otherstuff"
+			};
+			
+			tests.port(documentState)
+				.should.equal(3000);
+		});
+	});
 
-// var url			= require("url"),
-// 	querystring	= require("querystring");
-// 
-// module.exports = {};
-// 
-// 
-// // Node non-specific tests
-// 
-// module.exports.title = function(documentState) {
-// 
-// 	var $ = documentState.document,
-// 		title = $("title");
-// 
-// 	if (title.length)
-// 		return title.text();
-// 
-// 	return "";
-// };
-// 
-// module.exports.url = function(documentState) {
-// 	return documentState.url;
-// };
-// 
-// module.exports.domain = function(documentState) {
-// 	return url.parse(documentState.url).hostname;
-// };
-// 
-// module.exports.protocol = function(documentState) {
-// 	var protocol = url.parse(documentState.url).protocol;
-// 	return protocol.replace(/[^a-z0-9]/ig,"");
-// };
-// 
-// module.exports.port = function(documentState) {
-// 	return url.parse(documentState.url).protocol || 80;
-// };
-// 
-// module.exports.path = function(documentState) {
-// 	return url.parse(documentState.url).path;
-// };
-// 
-// module.exports.pathname = function(documentState) {
-// 	return url.parse(documentState.url).pathname;
-// };
-// 
-// module.exports.query = function(documentState,paramName) {
-// 	var query = url.parse(documentState.url).query;
-// 
-// 	if (!paramName) return query;
-// 
-// 	return querystring.parse(query)[paramName];
-// };
-// 
-// module.exports["status-code"] = function(documentState) {
-// 	if (!documentState.res.headers) return "";
-// 
-// 	return documentState.res.statusCode;
-// };
-// 
-// module.exports["content-length"] = function(documentState) {
-// 	if (!documentState.res.headers) return "";
-// 
-// 	return documentState.res.headers["content-length"];
-// };
-// 
-// module.exports["content-type"] = function(documentState) {
-// 	if (!documentState.res.headers) return "";
-// 
-// 	return documentState.res.headers["content-type"];
-// };
-// 
-// module.exports.header = function(documentState,headerName) {
-// 	return documentState.res.headers[headerName];
-// };
-// 
-// 
-// 
-// // Node specific tests
-// 
-// module.exports.required = function(documentState,node) {
-// 	if (node) return true;
-// 
-// 	return false;
-// };
-// 
-// module.exports.text = function(documentState,node) {
-// 	var $ = documentState.document;
-// 		node = $(node);
-// 
-// 	if (node.text())
-// 		return node.text();
-// 
-// 	return "";
-// };
-// 
-// module.exports.attribute = function(documentState,node,attribute) {
-// 	node = documentState.document(node);
-// 
-// 	if (!attribute) throw new Error("Attribute name required for test.");
-// 
-// 	if (!node) return false;
-// 
-// 	if (!node.attr(attribute)) return false;
-// 
-// 	return node.attr(attribute);
-// };
-// 
-// // Counts the number of nodes that matched a given selector.
-// 
-// module.exports.count = function(documentState,node,attribute) {
-// 	return documentState.selectionLength || 0;
-// };
+	describe("path",function() {
+		it("should return path component from the url of the document",function() {
+			var documentState = {
+				"url": "http://test.com:3000/fish?stuff=otherstuff"
+			};
+			
+			tests.path(documentState)
+				.should.equal("/fish?stuff=otherstuff");
+		});
+	});
+	
+	describe("pathname",function() {
+		it("should return path component (sans querystring) from the url of the document",function() {
+			var documentState = {
+				"url": "http://test.com:3000/fish?stuff=otherstuff"
+			};
+			
+			tests.pathname(documentState)
+				.should.equal("/fish");
+		});
+	});
+	
+	describe("query",function() {
+		it("should return querystring component from the url of the document",function() {
+			var documentState = {
+				"url": "http://test.com:3000/fish?stuff=otherstuff"
+			};
+			
+			tests.query(documentState)
+				.should.equal("stuff=otherstuff");
+		});
+	});
+	
+	describe("status-code",function() {
+		it("should return status-code of the request associated with the document",function() {
+			var documentState = {
+				res: {
+					"statusCode": 12345
+				}
+			};
+			
+			tests["status-code"](documentState)
+				.should.equal(12345);
+		});
+	});
+	
+	describe("content-length",function() {
+		it("should return the content length of the request associated with the document",function() {
+			var documentState = {
+				res: {
+					headers: {
+						"content-length": 12345
+					}
+				}
+			};
+			
+			tests["content-length"](documentState)
+				.should.equal(12345);
+		});
+	});
+	
+	describe("content-type",function() {
+		it("should return the content type header from the request associated with the document",function() {
+			var documentState = {
+				res: {
+					headers: {
+						"content-type": "application/xml+imaginary-format"
+					}
+				}
+			};
+			
+			tests["content-type"](documentState)
+				.should.equal("application/xml+imaginary-format");
+		});
+	});
+	
+	describe("header",function() {
+		it("should return the value of the specified header from the request associated with the document",function() {
+			var documentState = {
+				res: {
+					headers: {
+						"x-arbitrary-header": "You got me! Guess the jig is up.",
+						"header2": "And now for something completely different."
+					}
+				}
+			};
+			
+			tests.header(documentState,"x-arbitrary-header")
+				.should.equal("You got me! Guess the jig is up.");
+				
+			tests.header(documentState,"header2")
+				.should.equal("And now for something completely different.");
+			
+			chai.expect(tests.header(documentState,"nonexistent-header"))
+				.to.be.undefined;
+			
+		});
+	});
+	
+	describe("required",function() {
+		it("should return true if the node is present",function() {
+			var documentState = {
+				"document": cheerio.load("<html><title>Example title to extract</title></html>")
+			};
+			
+			var node = documentState.document("title").eq(0);
+			
+			tests.required(documentState,node)
+				.should.equal(true);
+		});
+	});
+	
+	describe("text",function() {
+		it("should return the text of the given node",function() {
+			var documentState = {
+				"document": cheerio.load("<html><title>Example title to extract</title></html>")
+			};
+			
+			var node = documentState.document("title").eq(0);
+			
+			tests.text(documentState,node)
+				.should.equal("Example title to extract");
+		});
+	});
+	
+	describe("attribute",function() {
+		it("should return the attribute value for a given attribute of a given node",function() {
+			var documentState = {
+				"document": cheerio.load("<html lang=en><title>Example title to extract</title></html>")
+			};
+			
+			var node = documentState.document("html").eq(0);
+			
+			// Successful attribute extraction
+			tests.attribute(documentState,node,"lang")
+				.should.equal("en");
+			
+			// Should throw error if we don't get an attribute
+			chai.expect(function() {
+					tests.attribute(documentState,node)
+				}).to.throw(Error);
+			
+			// Should return false if we don't have a node
+			tests.attribute(documentState,null,"test")
+				.should.be.false;
+			
+			// Should return false if this node doesn't have the attribute asked for...
+			tests.attribute(documentState,node,"test")
+				.should.be.false;
+		});
+	});
+	
+	describe("count",function() {
+		it("should return the number of nodes matched by a given selector",function() {
+			var documentState = {
+				"selectionLength": 20
+			};
+			
+			
+			tests.count(documentState)
+				.should.equal(20);
+			
+			tests.count({})
+				.should.equal(0);
+		});
+	});
+	
+});
