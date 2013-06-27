67471bf38815414c89cbe3ef09146981df70e426
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-06-27 16:04:49 +1000

    Added test-coverage script using istanbul

--- test/coverage.sh
+++ test/coverage.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-istanbul instrument lib -o lib-conv --no-compact &&
-	mv lib lib-uncov &&
+istanbul instrument lib -o lib-conv &&
+	mv lib lib-unconv &&
 	mv lib-conv lib &&
 	mocha -R mocha-istanbul -t 10000 &&
 	rm -rf lib &&
 	mv lib-unconv lib;
+
+if which open
+then
+	open html-report/index.html;
+fi
